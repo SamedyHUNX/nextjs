237b582727b76e311a042192bd385ce26a7992b8
samedyhunx <samedy.hunx@gmail.com>
2025-09-25 23:02:37 +0700

    master: addtion of folder 5

---
+++ 05-onwards-foodies-starting-project/app/meals/page.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function MealsPage() {
+  return (
+    <>
+      <h1>Welcome to mealp=\'dsafadjksdsfjkdsfdfsk;dfsLM</h1>;
+      <Link href="/meals/share">Share page</Link>
+    </>
+  );
+}
