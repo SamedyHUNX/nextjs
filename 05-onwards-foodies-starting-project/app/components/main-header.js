b340214af36343783fe18d5797aff85bfba62dd7
samedyhunx <samedy.hunx@gmail.com>
2025-09-25 23:28:42 +0700

    master: addtion of folder 5

--- 05-onwards-foodies-starting-project/app/components/main-header.js
+++ 05-onwards-foodies-starting-project/app/components/main-header.js
@@ -2,30 +2,34 @@ import Image from "next/image";
 import Link from "next/link";
 import logoImg from "./../../assets/logo.png";
 import classes from "./main-header.module.css";
+import MainHeaderBackground from "./main-header-background";
 
 export default function MainHeader() {
   return (
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <Image
-          src={logoImg.src}
-          alt="A plate with food on it"
-          width={100}
-          height={100}
-        />
-        NextLevel Food
-      </Link>
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image
+            src={logoImg.src}
+            alt="A plate with food on it"
+            width={100}
+            height={100}
+          />
+          NextLevel Food
+        </Link>
 
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Brown Meals</Link>
-          </li>
-          <li>
-            <Link href="/community">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Brown Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
